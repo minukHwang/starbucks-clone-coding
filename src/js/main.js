9b6273621f667df179c133f24097cd823a5d5d87
Minuk Hwang <damin2003@yonsei.ac.kr>
2022-05-21 16:55:50 +0900

    second commit

--- src/js/main.js
+++ src/js/main.js
@@ -39,3 +39,55 @@ fadeEls.forEach((fadeEl, index)=>{
         opacity:1,
     })
 })
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+});
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView:3, // 한번에 보여줄 슬라이드
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이가 가운데
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.swiper-next',
+    },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion")
+const swiperPagination = document.querySelector(".swiper-pagination")
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", ()=> {
+    isHidePromotion = !isHidePromotion
+    if(isHidePromotion) {
+        promotionEl.classList.add('hide');
+        swiperPagination.style.opacity = '0'
+    } else {
+        promotionEl.classList.remove('hide');
+        swiperPagination.style.opacity = '1'
+    }
+})
+
+function floatingObject(selector) {
+    gsap.to(selector, 1, {
+        y: 20,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        
+    })
+}
+
+floatingObject('.floating')
